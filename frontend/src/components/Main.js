2cb78fd171ba4b6beb98035cd726d96e7be58b19
Andrew McCann <mccann.an@husky.neu.edu>
2020-04-26 14:33:22 -0400

    Add placeholder card with tabs

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import CardWithTabs from './CardWithTabs/CardWithTabs';
 
-const Main = (props) =>
-  <div>
-    Hello world
-  </div>
+const tabs = [
+  {
+    title: 'Enter weight',
+    body: <React.Fragment>
+      Enter weight placeholder.
+    </React.Fragment>
+  },
+  {
+    title: 'Statistics',
+    body: <React.Fragment>
+      Statistics placeholder.
+    </React.Fragment>
+  }
+]
+
+const Main = (props) => {
+  return (
+    <CardWithTabs
+      tabs={tabs}/>
+  );
+};
 
 export default Main;
