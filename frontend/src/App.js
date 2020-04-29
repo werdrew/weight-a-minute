84fa14184aaa9069decebb5e5a334ecdfceaec61
Andrew McCann <mccann.an@husky.neu.edu>
2020-04-29 17:01:03 -0400

    Fix flex layout of App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import Main from './components/Main';
 import Logo from './components/Logo';
-import { Container, Row } from 'reactstrap';
+import { Container, Col, Row } from 'reactstrap';
 
 const today = new Date();
 const logoPath = "/logo_web.png";
 
 export default function App() {
   return (    
-    <Container fluid className="App p-0">
-      <div className="full-width top">
-        <Row className="justify-content-center">
-          <Logo path={logoPath}/>
-          <Main
-            date={today}/>
-        </Row>
-      </div>
-      <div className="full-width bottom"></div>
+    <Container fluid className="App p-0 full-width justify-content-center top">
+      <Row className="">
+        <Logo path={logoPath}/>
+      </Row>
+      <Row>
+        <Main
+          date={today}/>
+      </Row>
     </Container>
   );
 };
